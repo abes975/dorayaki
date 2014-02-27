4d62e60c4207ca468afa441d3c617eb730f56d2f
Seba <abes975@gmail.com>
2014-02-28 00:20:17 +0100

    fixed lenght shift..was in the wrong direction...still does not work

--- src/udp_broker.h
+++ src/udp_broker.h
@@ -2,7 +2,7 @@
 #define __DORAYAKI_UDP_BROKER__
 
 #include "socket_pool.h"
-#define MAX_DNS_UDP 2 >> 12
+#define MAX_DNS_UDP 2 << 12
 
 typedef struct udp_broker {
     int listen_sock; /*!< Listening upd socket fd... */
